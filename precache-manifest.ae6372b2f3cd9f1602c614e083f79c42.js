d8fabcb7c1cd01e6c9ddd3c7af25b34df5c3d7fd
luis-f-lins <luisfernandolins@gmail.com>
2020-04-06 17:01:44 -0300

    Updates

---
+++ precache-manifest.ae6372b2f3cd9f1602c614e083f79c42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c7cb5138ce0ecbcf5b202f9f22ff2d",
+    "url": "/bookmark-manager-landing-page/index.html"
+  },
+  {
+    "revision": "ec2659f8355612549261",
+    "url": "/bookmark-manager-landing-page/static/css/main.370c7d0c.chunk.css"
+  },
+  {
+    "revision": "8b3aa051a6cefef216b7",
+    "url": "/bookmark-manager-landing-page/static/js/2.3d8b8701.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookmark-manager-landing-page/static/js/2.3d8b8701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2659f8355612549261",
+    "url": "/bookmark-manager-landing-page/static/js/main.ef0ef21d.chunk.js"
+  },
+  {
+    "revision": "17eb0da7e400d47260ec",
+    "url": "/bookmark-manager-landing-page/static/js/runtime-main.c08e033d.js"
+  },
+  {
+    "revision": "dfa0cd821469019e297c8c27cfc0d475",
+    "url": "/bookmark-manager-landing-page/static/media/illustration-hero.dfa0cd82.svg"
+  },
+  {
+    "revision": "53989d2a42baf707e1d92f5d05c7899b",
+    "url": "/bookmark-manager-landing-page/static/media/logo-bookmark.53989d2a.svg"
+  }
+]);
